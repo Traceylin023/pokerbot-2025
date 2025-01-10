232a7b6fc8e8a68ff24ba62f2c01922f675d8558
Tracey Lin <tlin41@stuy.edu>
2025-01-09 20:44:15 -0500

    added monte carlo and pot odds basic version

--- python_skeleton/player.py
+++ python_skeleton/player.py
@@ -1,6 +1,7 @@
 '''
 Simple example pokerbot, written in Python.
 '''
+import eval7
 from skeleton.actions import FoldAction, CallAction, CheckAction, RaiseAction
 from skeleton.states import GameState, TerminalState, RoundState
 from skeleton.states import NUM_ROUNDS, STARTING_STACK, BIG_BLIND, SMALL_BLIND
@@ -77,6 +78,38 @@ class Player(Bot):
         if opponent_bounty_hit:
             print("Opponent hit their bounty of " + opponent_bounty_rank + "!")
 
+    def calculate_strength(self, my_cards, board_cards):
+        print(f"my cards: {my_cards}")
+        print(f"board card: {board_cards}")
+        MC_ITER = 100
+        my_cards = [eval7.Card(card) for card in my_cards]
+        board_cards = [eval7.Card(card) for card in board_cards]
+        deck = eval7.Deck()
+        for card in my_cards + board_cards:
+            deck.cards.remove(card)
+        
+        score = 0
+        for _ in range(MC_ITER):
+            deck.shuffle()
+            draw_number = 2 + (5 - len(board_cards))
+            draw = deck.peek(draw_number)
+            opp_draw = draw[:2]
+            board_draw = draw[2:]
+            my_hand = my_cards + board_cards + board_draw
+            opp_hand = opp_draw + board_cards + board_draw
+            my_value = eval7.evaluate(my_hand)
+            opp_value = eval7.evaluate(opp_hand)
+            if my_value > opp_value:
+                score += 1
+            elif my_value < opp_value:
+                score += 0
+            else:
+                score += 0.5
+        
+        win_rate = score / MC_ITER
+        print(f"win rate: {win_rate}")
+        return win_rate
+
     def get_action(self, game_state, round_state, active):
         '''
         Where the magic happens - your code should implement this function.
@@ -109,7 +142,9 @@ class Player(Bot):
            min_cost = min_raise - my_pip  # the cost of a minimum bet/raise
            max_cost = max_raise - my_pip  # the cost of a maximum bet/raise
         if RaiseAction in legal_actions:
-            if random.random() < 0.5:
+            win_rate = self.calculate_strength(my_cards, board_cards)
+            pot_odds = continue_cost / (my_pip + opp_pip + 0.1)
+            if win_rate > 0.6 and win_rate > pot_odds:
                 return RaiseAction(min_raise)
         if CheckAction in legal_actions:  # check-call
             return CheckAction()
